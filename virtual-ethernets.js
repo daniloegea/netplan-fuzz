55d02100177d78f39a725359626f6c07089b45ce
Danilo Egea Gondolfo <danilogondolfo@gmail.com>
2024-04-03 15:24:05 +0100

    virtual-ethernets: refactoring
    Always generate a valid veth pair with the peer property.

--- virtual-ethernets.js
+++ virtual-ethernets.js
@@ -1,5 +1,29 @@
 import * as common from "./common.js";
 
+const common_veth_properties = {
+    optional: {
+        type: "boolean"
+    },
+    macaddress: {
+        type: "string",
+        faker: "internet.mac"
+    },
+    "ipv6-privacy": {
+        type: "boolean"
+    },
+    "link-local": {
+        type: "array",
+        items: [{
+            type: "string",
+            enum: ["ipv4", "ipv6"],
+        }
+        ]
+    },
+    "ignore-carrier": {
+        type: "boolean"
+    },
+}
+
 const virtual_ethernets_schema = {
     type: "object",
     additionalProperties: false,
@@ -26,46 +50,48 @@ const virtual_ethernets_schema = {
                         },
                     },
                     patternProperties: {
+                        "veth0": {
+                            additionalProperties: false,
+                            required: ["peer"],
+                            properties: {
+                                peer: {
+                                    type: "string",
+                                    enum: ["veth1"],
+                                },
+                                ...common.common_properties,
+                                ...common_veth_properties,
+                                ...common.networkmanager_settings,
+                                ...common.openvswitch
+                            },
+                        },
+                        "veth1": {
+                            additionalProperties: false,
+                            required: ["peer"],
+                            properties: {
+                                peer: {
+                                    type: "string",
+                                    enum: ["veth0"],
+                                },
+                                ...common.common_properties,
+                                ...common_veth_properties,
+                                ...common.networkmanager_settings,
+                                ...common.openvswitch
+                            },
+                        },
                         "[azAZ09-]{1,15}": {
                             additionalProperties: false,
                             properties: {
-                                ...common.common_properties,
                                 peer: {
                                     type: "string",
                                 },
-                                optional: {
-                                    type: "boolean"
-                                },
-                                macaddress: {
-                                    type: "string",
-                                    faker: "internet.mac"
-                                },
-                                /*
-                                "ipv6-mtu": {
-                                    type: "integer",
-                                    minimum: 0
-                                },
-                                */
-                                "ipv6-privacy": {
-                                    type: "boolean"
-                                },
-                                "link-local": {
-                                    type: "array",
-                                    items: [{
-                                        type: "string",
-                                        enum: ["ipv4", "ipv6"],
-                                    }
-                                    ]
-                                },
-                                "ignore-carrier": {
-                                    type: "boolean"
-                                },
+                                ...common.common_properties,
+                                ...common_veth_properties,
                                 ...common.networkmanager_settings,
                                 ...common.openvswitch
                             },
                         }
                     },
-                    required: ["[azAZ09-]{1,15}"]
+                    required: ["[azAZ09-]{1,15}", "veth0", "veth1"]
                 },
             },
             required: ["virtual-ethernets"]
